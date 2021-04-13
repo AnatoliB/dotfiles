e1736a18522c717db56df1db37e9fff5cf3f7388
Anatoli Beliaev <anatolib@microsoft.com>
2021-04-12 21:54:16 -0700

    Add logs

--- install.ps1
+++ install.ps1
@@ -1,9 +1,16 @@
+$logFileName = "~/dotfiles-install-ps1.log"
+Write-Verbose 'Starting install.ps1' -Verbose 4>&1 > $logFileName
+
 $configPath = (Join-Path $PSScriptRoot '.config')
 
 # Set up symlinks
 Get-ChildItem -Path $configPath | ForEach-Object {
-    New-Item -Path (Join-Path '~/.config' $_.Name) -ItemType SymbolicLink -Value $_.FullName
+    $path = Join-Path '~/.config' $_.Name
+    Write-Verbose "Creating symlink from $path to $($_.FullName)" -Verbose 4>&1 >> $logFileName
+    New-Item -Path $path -ItemType SymbolicLink -Value $_.FullName -Verbose 4>&1 >> $logFileName
 }
 
-Install-Module PSDepend -Force
-Invoke-PSDepend -Force (Join-Path $PSScriptRoot 'requirements.psd1')
+Install-Module PSDepend -Force -Verbose 4>&1 >> $logFileName
+Invoke-PSDepend -Force (Join-Path $PSScriptRoot 'requirements.psd1') -Verbose 4>&1 >> $logFileName
+
+Write-Verbose 'Exiting install.ps1' -Verbose 4>&1 >> $logFileName
