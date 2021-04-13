3b0cf4b8fbac591ebac6f2c60abf4814b46799d1
Anatoli Beliaev <anatolib@microsoft.com>
2021-04-12 22:24:36 -0700

    Add logs

--- install.ps1
+++ install.ps1
@@ -1,15 +1,18 @@
 $logFileName = "~/dotfiles-install-ps1.log"
 Write-Verbose 'Starting install.ps1' -Verbose 4>&1 > $logFileName
 
-$configPath = (Join-Path $PSScriptRoot '.config')
+$configPath = Join-Path $PSScriptRoot '.config'
+
+$homeConfigPath = Join-Path '~' '.config'
+Write-Verbose "Directory $homeConfigPath $((Test-Path -Path $homeConfigPath) ? 'exists' : 'does not exist')" -Verbose 4>&1 > $logFileName
 
 # Set up symlinks
 Get-ChildItem -Path $configPath | ForEach-Object {
-    $path = Join-Path '~/.config' $_.Name
+    $path = Join-Path $homeConfigPath $_.Name
     Write-Verbose "Creating symlink from $path to $($_.FullName)" -Verbose 4>&1 >> $logFileName
     New-Item -Path $path -ItemType SymbolicLink -Value $_.FullName -Verbose 2>&1 4>&1 >> $logFileName
     Write-Verbose '-----' -Verbose 4>&1 >> $logFileName
-    Get-ChildItem -Path ~/.config >> $logFileName
+    Get-ChildItem -Path $homeConfigPath >> $logFileName
     Write-Verbose '-----' -Verbose 4>&1 >> $logFileName
 }
 
